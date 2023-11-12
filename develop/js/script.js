0949849da98b1632d3d147a68c339db56577c289
John Lopez <purplejohn13@gmail.com>
2023-11-11 19:02:58 -0800

    added error message for no input

---
+++ develop/js/script.js
@@ -0,0 +1,15 @@
+var searchFormEl = document.querySelector('#search-form');
+
+ function searchRequest(event) {
+
+    event.preventDefault();
+
+    var searchInput = document.querySelector('#search-input');
+    if (!searchInput) {
+        console.error('No search input');
+        return;
+    }
+
+    
+
+ }
