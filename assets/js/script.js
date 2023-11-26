bb90a67043fa90dbca3ec67c0be3cc5808bf009b
John Lopez <purplejohn13@gmail.com>
2023-11-26 14:47:18 -0800

    added variables in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,6 @@
-var searchFormEl = document.querySelector('#search-form');
+var cityName = document.querySelector('#cityname');
+var btn = document.querySelector('.btn');
+var divData = document.querySelector('#search-history');
+var index = 1;
 
-function searchRequest(event) {
 
-    event.preventDefault();
-
-    var searchInput = document.querySelector('#search-input');
-    if (!searchInput) {
-        console.error('No search input');
-        return;
-    }
-
-}
-
-function getApi() {
-    var requestUrl = 'api.openweathermap.org/data/2.5/forecast?lat={32.715736}&lon={-117.161087}&appid={fe1420d80f70063f112b634076677a72}';
-
-    fetch(requestUrl).then(function (response) {
-        return response.json();
-    })
-}
-console.log(getApi());
